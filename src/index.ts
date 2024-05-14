7399817525d1e87914dc94b5ef48f1b4c512fbcb
Enrico Bono <me@enricobono.it>
2024-05-14 10:07:00 +0200

    Add config

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ import type { ApiConfig } from "./utils/interfaces";
 const { port } = config.get<ApiConfig>("api");
 
 export const server = app.listen(port, () => {
-  console.log(`API listening on http://localhost:${port}`);
-  console.log(`Usage: Open http://localhost:${port} in your browser.`);
+  console.log(`API listening on host:${port}`);
+  console.log(`Usage: Open host:${port} in your browser.`);
 });
